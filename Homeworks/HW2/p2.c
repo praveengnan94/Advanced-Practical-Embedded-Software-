3b661702655e241e2de0c08a7341e4fe0dfefd03
praveengnan94 <praveengnan94@gmail.com>
2018-02-01 16:34:15 -0700

    p2 almost complete

--- Homeworks/HW2/p2.c
+++ Homeworks/HW2/p2.c
@@ -11,18 +11,78 @@ int main(int argc, char const *argv[])
 	int file_ret; //return type of file system calls
 	char filename[20]="testfile.txt";
 	char cwd[1024];
-   if (getcwd(cwd, sizeof(cwd)) == NULL)
-       perror("getcwd() error");
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	   perror("getcwd() error");
 
 	strcat(cwd,"/");
 	strcat(cwd,filename);
 	printf("Some interesting output %s\n",cwd);
 
-	file_ret=open(cwd,O_CREAT); //if file doesnt exist, create it. open for read/write and append to end of file
+	file_ret=open(cwd,O_CREAT); //if file doesnt exist, create it
+
+	if(file_ret==-1)
+		perror("File could not be opened");
+	if(close(file_ret)==-1)
+		perror("File could not be closed");
+
+	if(chmod (filename,S_IROTH|S_IWOTH)==-1) //modify file permission to be read/write
+		perror("Cannot change file permission");
+
+	file_ret=open(cwd,O_WRONLY); //open file for writing
+
+	if(write(file_ret,"a",sizeof(char))==-1) //write a character to the file
+		perror("Cannot write a character to the file");
+	if(close(file_ret)==-1)
+		perror("File could not be closed");
+
+	file_ret=open(cwd,O_APPEND|O_RDWR); //open file in append mode
+
+	if(file_ret==-1)
+		perror("File could not be opened");
+	char *dynamic_mem= (char*)malloc(200);
+
+	printf("Enter a string to write to the file\n");
+
+	scanf("%s",dynamic_mem);
+
+	if(write(file_ret,dynamic_mem,strlen(dynamic_mem))==-1)
+		perror("Cannot write string to the file");
+
+	
+	file_ret=open(cwd,O_RDONLY);
 	if(file_ret==-1)
 		perror("File could not be opened");
 
-	// if(chmod (filename,S_IROTH|S_IWOTH)==-1) //modify file permission to be read/write
-	// 	perror("Cannot change file permission");
+	char read_file[200];
+	int ret_val;
+
+	printf("Flushing file output\n");
+
+	while(ret_val=read(file_ret,read_file,1)){
+		if(ret_val==-1)
+			perror("Cannot read contents from file");				
+		printf("%c",*read_file);
+	}
+
+	printf("\n");
+	
+	if(close(file_ret)==-1)
+		perror("File could not be closed");
+
+	// file_ret=open(cwd,O_RDONLY);
+	FILE *fp = fopen(cwd,"r+");
+	if(file_ret==-1)
+		perror("File could not be opened");
+
+	printf("single character %c\n",getc(fp));
+
+	printf("string of characters %s\n",gets(fp));
+
+	if(fclose(fp)==-1)
+		perror("File could not be closed");
+
+	free(dynamic_mem); //free the allocated memory
+	
 	return 0;
 }
+ 
